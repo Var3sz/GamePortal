e96290190ab581300d3c477c285b362145c436c5
Var3sz <varga.adam011208@gmail.com>
2023-11-08 20:29:29 +0100

    Role repository

---
+++ GamePortal/Repository/IRoleRepository.cs
@@ -0,0 +1,11 @@
+﻿using GamePortal.Models;
+
+namespace GamePortal.Repository
+{
+    public interface IRoleRepository
+    {
+        List<Role> GetRoles();
+        Role GetRoleByName(string role);
+        Role GetRoleById(int id);
+    }
+}
---
+++ GamePortal/Repository/RoleRepository.cs
@@ -0,0 +1,29 @@
+﻿using GamePortal.Models;
+
+namespace GamePortal.Repository
+{
+    public class RoleRepository : IRoleRepository
+    {
+        private readonly GamePortalDbContext _dbContext;
+
+        public RoleRepository(GamePortalDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Role GetRoleById(int id)
+        {
+            return _dbContext.Roles.SingleOrDefault(q => q.RoleId == id)!;
+        }
+
+        public Role GetRoleByName(string role)
+        {
+            return _dbContext.Roles.SingleOrDefault(q => q.Name == role)!;
+        }
+
+        public List<Role> GetRoles()
+        {
+            return _dbContext.Roles.ToList();
+        }
+    }
+}
